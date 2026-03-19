98471c5c3549fc6fed73111f5b1bdafafebee3a2
Jacopo <jacopo.bonotto17@gmail.com>
2026-03-19 13:51:09 +0100

    First forecast using Naive (Mean) Forecast

--- BTP_BUND_Spread.R
+++ BTP_BUND_Spread.R
@@ -1,3 +1,49 @@
 
+remove(list = ls())
 
-a <- 1
+install.packages("readxl")
+library(readxl)
+
+# Load the data from excel file, weekly spread 
+
+data <- read_excel("Spread-weekly.xlsx")
+
+# Remove first row, as it contains NA
+data <- data[-1,]
+
+# The data is inverse, we need to change sign
+data$`IT10DE10=TWEB (BID)` <- as.numeric(data$`IT10DE10=TWEB (BID)`) * -1
+
+# Inspect data
+head(data)
+str(data)
+
+# parse the data
+
+library(zoo)
+
+data$Date <- as.Date(data$Date)
+spread <- zoo(data$`IT10DE10=TWEB (BID)`, order.by = data$Date)
+
+# Plot the data
+
+plot(spread, main="BTP-BUND Spread (Weekly", ylab="Spread (bps)", xlab= "Date")
+
+
+
+# Forecast #1, using Naive Forecast (Mean)
+
+mean <- mean(data$`IT10DE10=TWEB (BID)`, na.rm=TRUE)
+
+# Plot spread values with the mean value
+
+plot(spread, main="BTP-BUND Spread (Weekly", ylab="Spread (bps)", xlab= "Date")
+abline(h = mean, col="red", lwd=2, lty=2)
+text(x = start(spread), y= mean,
+     labels=paste("Mean =", round(mean, 2)),
+     col="red", adj= c(0, -0.5))
+
+
+
+
+
